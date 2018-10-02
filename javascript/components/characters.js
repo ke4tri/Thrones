ad61abb835cdc8173b425b387798fb5f1a63ef12
Wayne Collier <ke4tri@gmail.com>
2018-10-01 20:31:52 -0500

    character array

---
+++ javascript/components/characters.js
@@ -0,0 +1,26 @@
+const characters = [
+    {
+        name:'Jon Snow', 
+        house:'Stark',
+        image:'https://s3.r29static.com//bin/entry/4f0/720x864,85/1801540/image.jpg'
+    },
+    {
+        name:'Daenerys Targaryen', 
+        house:'Targaryen',
+        image:'https://amp.thisisinsider.com/images/5989fc4eefe3df1f008b48b9-750-563.png'
+    },
+    {
+        name:'Cersei Lannister', 
+        house:'Lannister',
+        image:'http://pixel.nymag.com/imgs/daily/vulture/2015/04/09/09-cersei-lannister.w529.h352.jpg'
+    },
+    {
+        name:'Tyrion Lannister', 
+        house:'Lannister',
+        image:'https://amp.thisisinsider.com/images/59a5dd9cb065da49008b4878-750-563.png'
+    }
+
+]
+
+
+export{characters}
