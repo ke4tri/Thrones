4ddedb91bebdb7ab86c52fa12e3c2a4e41477a20
Wayne Collier <ke4tri@gmail.com>
2018-10-03 06:43:54 -0500

    had minor issue and seemed imposible to find

--- javascripts/data/charactersData.js
+++ javascripts/data/charactersData.js
@@ -1,22 +1,21 @@
-import {setCharacters, charactersBuilder} from '../components/characters.js';
+import {setCharacters, charactersBuilder, getCharacterz} from '../components/characters.js';
 
 function executeThisCodeAfterFileLoaded () {
-    const data = JSON.parse(this.responseText);
-    setCharacters(data.characters);
-    charactersBuilder();
-    console.log('request', data.characters);
+  const data = JSON.parse(this.responseText);
+  setCharacters(data.characters);
+  charactersBuilder(getCharacterz());
 }
 
 function executeThisCodeIfXhrFails () {
-    console.log("Shit don't do Good");
+  console.log('shit broke');
 }
 
 const getCharacters = () => {
-    let myRequest = new XMLHttpRequest();
-    myRequest.addEventListener('load', executeThisCodeAfterFileLoaded);
-    myRequest.addEventListener('error',executeThisCodeIfXhrFails);
-    myRequest.open('GET', './db/characters.json');
-    myRequest.send();
+  let myRequest = new XMLHttpRequest();
+  myRequest.addEventListener('load', executeThisCodeAfterFileLoaded);
+  myRequest.addEventListener('error', executeThisCodeIfXhrFails);
+  myRequest.open('GET', './db/characters.json');
+  myRequest.send();
 };
 
 export {getCharacters};
--- javascripts/helpers/util.js
+++ javascripts/helpers/util.js
@@ -1,6 +1,6 @@
 const printToDom = (stringToPrint) => {
-    const printHere = document.getElementById('characters');
-    printHere.innerHTML = stringToPrint;
-  }
-  
-  export {printToDom};
+  const printHere = document.getElementById('characters');
+  printHere.innerHTML = stringToPrint;
+}
+
+export {printToDom};
